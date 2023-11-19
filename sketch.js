0c749ac9a0038d3b8adb92600ef7b92fe3c73e29
lovetheland <32654329+lovetheland@users.noreply.github.com>
2023-11-19 03:31:25 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -31,6 +31,7 @@ function setup() {
   
   createCanvas(canx, cany, WEBGL);
   rensphere = createGraphics(canx, cany, WEBGL);
+  rensphere2 = createGraphics(canx, cany, WEBGL);
   renblurh2 = createGraphics(canx, cany, WEBGL); //
   renblurv2 = createGraphics(canx, cany, WEBGL); //
   rendisp2 = createGraphics(planex, planex, WEBGL); //
@@ -39,17 +40,17 @@ function setup() {
 function draw() {
 //  console.log(frameRate());
   
-  push();
-  rotateY(frameCount * -0.0005 + 20);
-  texture(img);
-  sphere(1000);
-  pop();
-  
   rensphere.clear();
   rensphere.texture(img);
   rensphere.strokeWeight(.65);
   rensphere.sphere(1000);
-  rensphere.rotateY(.001);
+  rensphere.rotateY(.002);
+  
+  rensphere2.clear();
+  rensphere2.texture(img);
+  rensphere2.noStroke();
+  rensphere2.sphere(1000);
+  rensphere2.rotateY(-.001);
   
   renblurh2.shader(shaderblurh2);
   shaderblurh2.setUniform('tex0', rensphere);
@@ -60,14 +61,14 @@ function draw() {
   shaderblurv2.setUniform('tex0', renblurh2);
   shaderblurv2.setUniform('texelSize', [1.0/canx, 1.0/cany]);
   shaderblurv2.setUniform('direction', [1.0, 0.0]);
-  renblurv2.rect(0, 0, -planex, -planey);
+  renblurv2.rect(0, 0, planex, planey);
   
   rendisp2.shader(shaderdisp);
   shaderdisp.setUniform('tex0', renblurv2);
-  shaderdisp.setUniform('tex1', rensphere);
+  shaderdisp.setUniform('tex1', rensphere2);
   shaderdisp.setUniform('amt', .02);
   rendisp2.rect(0, 0, -canx, -cany);
 
-  
-  image(renblurv2, -planex/2, -planey/2, planex, planey);
+  image(rensphere, -canx/2, -cany/2, canx, cany);
+  image(rendisp2, -planex/2, -planey/2, planex, planey);
 }
