3a87806e16755c118778a8a1f47d630bb4e49cb5
lovetheland <32654329+lovetheland@users.noreply.github.com>
2023-11-19 16:12:21 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -31,7 +31,9 @@ function setup() {
   createCanvas(canx, cany, WEBGL);
   rensphere = createGraphics(canx, cany, WEBGL);
   rensphere2 = createGraphics(canx/4, cany/4, WEBGL);
-  rendisp2 = createGraphics(planex, planex, WEBGL); //
+  rendisp2 = createGraphics(planex, planex, WEBGL);
+  
+
 }
 
 function draw() {
@@ -43,21 +45,21 @@ function draw() {
   rensphere.clear();
   rensphere.texture(img);
   rensphere.sphere(1500);
-  rensphere.rotateY(.0005);
+  rensphere.rotateY(-.0005);
   
   rensphere2.clear();
   rensphere2.texture(blurimg);
   rensphere2.noStroke();
 //  rensphere2.strokeWeight(.65);
   rensphere2.sphere(1500);
-  rensphere2.rotateY(-.0001);
+  rensphere2.rotateY(.0001);
   
   image(rensphere, -canx/2, -cany/2, canx, cany);
   
   rendisp2.shader(shaderdisp);
   shaderdisp.setUniform('tex0', rensphere2);
   shaderdisp.setUniform('tex1', rensphere);
-  shaderdisp.setUniform('amt', .025);
+  shaderdisp.setUniform('amt', .02);
   rendisp2.rect(0, 0, -canx, -cany);
 
   image(rendisp2, -planex/2, -planey/2, planex, planey);
